5a998f324ba6cdfc4ee3d4756db5477bf3dee64a
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-04-25 13:54:39 +0300

    Project Created

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Ömer Oğuz Çelikel on 25.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
