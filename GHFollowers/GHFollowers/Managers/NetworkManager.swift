6423ba62d5c3620b82394735c6b9a90db369253b
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-05-12 10:24:43 +0300

    Result Type Refactor

--- GHFollowers/GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/GHFollowers/Managers/NetworkManager.swift
@@ -13,14 +13,15 @@ class NetworkManager {
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
+    func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
         let endpoint = baseURL + "\(username)/followers?per_page=100&\(page)"
         
         guard let url = URL(string: endpoint) else {
             //Should return an error
             //if call error from here it will be background thread
             //pass error message to ViewController it will present it
-            completed(nil, .invalidUsername)
+//            completed(nil, .invalidUsername)
+            completed(.failure(.invalidUsername))
             return
         }
         
@@ -28,17 +29,17 @@ class NetworkManager {
             //nothing will be happen. Mostly internet connection. 
             //If there is an error from API it will be like 404 error in response
             if let _ = error {
-                completed(nil, .unableToComplete)
+                completed(.failure(.unableToComplete))
             }
             
             // response not nill and check satus code
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, .invalidResponse)
+                completed(.failure(.invalidResponse))
                 return
             }
             
             guard let data = data else {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
                 return
             }
             
@@ -46,10 +47,11 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers, nil)
+                //completed(followers, nil)
+                completed(.success(followers))
             } catch {
 //                completed(nil, error.localizedDescription) // good error message to the developers
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
             }
         }
         
--- GHFollowers/GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/GHFollowers/Screens/FollowerListVC.swift
@@ -16,15 +16,25 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case.failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+        /*
         NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
             guard let followers = followers else {
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage!.rawValue, buttonTitle: "Ok")
                 return
             }
-            print("Followers.count = \(followers.count)")
             print("Followers= \(followers)")
         }
+         */
     }
+         
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
