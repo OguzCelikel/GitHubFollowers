4d2cf4502215761a7068c7ae3fa0b3d69a6af245
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-05-15 10:50:14 +0300

    Pagination - Getting the Next 100 Followers

--- GHFollowers/GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/GHFollowers/Managers/NetworkManager.swift
@@ -9,13 +9,13 @@ import UIKit
 
 class NetworkManager {
     static let shared   = NetworkManager()
-    private let baseURL         = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
     let cache           = NSCache<NSString, UIImage>()
     
     private init() {}
     
     func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
-        let endpoint = baseURL + "\(username)/followers?per_page=100&\(page)"
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
             //Should return an error
