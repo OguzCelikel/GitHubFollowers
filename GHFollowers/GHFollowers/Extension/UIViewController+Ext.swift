db942117e0e92ceb91bc2fc92ab579ea2e2ffa57
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-05-16 16:10:24 +0300

    Loading View

--- GHFollowers/GHFollowers/Extension/UIViewController+Ext.swift
+++ GHFollowers/GHFollowers/Extension/UIViewController+Ext.swift
@@ -5,8 +5,9 @@
 //  Created by Ömer Oğuz Çelikel on 6.05.2024.
 //
 
-import Foundation
 import UIKit
+
+fileprivate var containerView: UIView!
 
 extension UIViewController {
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
@@ -15,6 +16,35 @@ extension UIViewController {
             alertVC.modalPresentationStyle  = .overFullScreen
             alertVC.modalTransitionStyle    = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha           = 0
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dissmissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
