b736d47310d895acdbff4a8b29a3dfff5d267ee7
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-05-25 09:56:43 +0300

    Child View Controller - UserInfoHeaderVC

---
+++ GHFollowers/GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Ömer Oğuz Çelikel on 24.05.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
