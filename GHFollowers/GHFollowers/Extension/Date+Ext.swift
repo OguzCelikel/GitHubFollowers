162f7ecc8b5c44cee3ecf2c1a59750acbd07e30d
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-05-26 07:44:46 +0300

    Date Conversions & Date Label

---
+++ GHFollowers/GHFollowers/Extension/Date+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  Date+Ext.swift
+//  GHFollowers
+//
+//  Created by Ömer Oğuz Çelikel on 25.05.2024.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
